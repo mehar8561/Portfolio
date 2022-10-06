48f4c646c20eaeaf1b3974958bff8c45e94998f2
mehar <mehar8561@gmail.com>
2022-10-06 17:08:18 -0400

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -37,7 +37,9 @@ function ResumeNew() {
 
           <Document file={pdf} className="d-flex justify-content-center">
             
-          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6}/>
+          <Page pageNumber={1} />
+          <Page pageNumber={2} />
+          {/* <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6}/> */}
           </Document>
           {/* <Document file={resumeLink} className="d-flex justify-content-center">
             
